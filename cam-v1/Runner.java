4965bae11ed5cd282b6c87bb4344f1c86361c9d4
Aarohank <130250136+Aarohank@users.noreply.github.com>
2023-10-20 21:27:47 -0700

    Add files via upload
    Final

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,18 +1,18 @@
+import java.util.Random;
+
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import java.lang.Math;
-import java.util.Random;
 
 public class Runner{
     VirtualPet p = new VirtualPet();
     public Runner(){
-        System.out.println(randy());
+        System.out.println(randy(100));
         p.exercise();
         takeABeat(1000);
         p.feed();
-        getAnswer("how do you say nosotros in english");
+        getAnswer("If you say 'among' or 'us' here it will swtich its face");
         p.over(getBoolean("Is it over?"));
-        System.out.println(randy());
+        numberGuessing();
     }
     
     public void takeABeat(int milliseconds){
@@ -22,6 +22,38 @@ public class Runner{
 
         }
     }
+    public int randy(int max){
+        Random rand = new Random();
+        int int_random = rand.nextInt(max);
+        return int_random;
+    }
+    public void numberGuessing(){
+        if (getBoolean("do you want to play a game")){
+        p.ecstatic();
+        int x = randy(getInt("What is the max number"));
+        int g = -1;
+        int c = 0;
+        while (x != g){
+            int t = getInt("Guess a number");
+            if  (t != x){
+                if (t > x){
+                    p.high();
+                    c++;
+                    p.changeFace(p ,c);
+                }
+                else{
+                    p.low();
+                    c ++;
+                    p.changeFace(p ,c);
+                }
+            }
+            else{
+            p.win(c,x );
+            }
+            }
+        }
+        else p.enraged();
+    }
 
     public String getAnswer(String q){
         String s = (String)JOptionPane.showInputDialog(
@@ -29,22 +61,22 @@ public class Runner{
             q,
             "String Question?",
             JOptionPane.PLAIN_MESSAGE
-       );
-       if (p.Mogus(s)){
+    );
+    if (p.Mogus(s)){
                 p.Sus();
                 takeABeat(1000);
         }
-       return s;
+    return s;
     }
 
     public int getInt(String q){
     
         String s = (String)JOptionPane.showInputDialog(
-         new JFrame(),
-         q,
-         "Integer Question?",
-         JOptionPane.PLAIN_MESSAGE
-       );
+        new JFrame(),
+        q,
+        "Integer Question?",
+        JOptionPane.PLAIN_MESSAGE
+    );
     int x = Integer.parseInt(s);
     return x;
     }
@@ -52,23 +84,24 @@ public class Runner{
     public boolean getBoolean(String q){
     
         String s = (String)JOptionPane.showInputDialog(
-         new JFrame(),
-         q,
-         "Boolean Question?",
-         JOptionPane.PLAIN_MESSAGE
-       );
+        new JFrame(),
+        q,
+        "Boolean Question?",
+        JOptionPane.PLAIN_MESSAGE
+    );
     if (s.equals("yes")){
         s="true";
     }
+    if (s.equals("no")){
+        s="false";
+    }
     boolean x = Boolean.parseBoolean(s);
     return x;
+    }
     
-    }
-    public int randy(){
-        Random rand = new Random(); 
-        int int_random = rand.nextInt(100);
-        return int_random;
-    }
+    
+    
+    
     public static void main(String[] args) {
         new Runner();
     }
--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -3,6 +3,7 @@
  * @author Cam
  * @author ?
  */
+
 public class VirtualPet {
     
     VirtualPetFace face;
@@ -60,5 +61,69 @@ public class VirtualPet {
             face.setMessage("AMONG US");
             face.setImage("sus");
     }
+    
+    public void ecstatic(){
+        face.setImage("ecstatic");
+    }
 
-} // end Virtual Pet
+    public void low(){
+        face.setMessage("Too low, try again");
+    }
+
+    public void high(){
+        face.setMessage("Too high, try again");
+    }
+    public void win(int c, int x) {
+        face.setMessage("Congradulations, you got the number " + x + " after " + c + " tries");
+        face.setImage("astonished");
+    }
+    public void enraged(){
+        face.setImage("enraged");
+    }
+
+    public void joyful(){
+        face.setImage("joyful");
+    }
+    public void happy(){
+        face.setImage("happy");
+    }
+    public void normal(){
+        face.setImage("normal");
+    }
+    public void sad(){
+        face.setImage("sad");
+    }
+    public void pe(){
+        face.setImage("pe");
+    }
+    public void vsad(){
+        face.setImage("verysad");
+    }
+    public void depressed(){
+        face.setImage("depressed");
+    }
+    public void skeleton(){
+        face.setImage("skeleton");
+    }
+    public void pushingdaisies(){
+        face.setImage("pushingdaisies");
+    }
+    public void angel(){
+        face.setImage("angel");
+    }
+
+    public void changeFace(VirtualPet x, int c){
+        if (c == 1) x.joyful();
+        if (c == 2) x.happy();
+        if (c == 3) x.normal();
+        if (c == 4) x.sad();
+        if (c == 5) x.pe();
+        if (c == 6) x.vsad();
+        if (c == 7) x.depressed();
+        if (c == 8) x.skeleton();
+        if (c == 9) x.pushingdaisies();
+        if (c == 10) x.angel();
+    }
+    
+}
+ // end Virtual Pet
